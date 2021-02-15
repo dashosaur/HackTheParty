16a70729073ef209eaa49e21e1d45f6fd558a2b9
Dash Brittain <roxanne.brittain@gmail.com>
2021-02-15 12:45:08 -0800

    Fix Da Vinci

--- da_vinci.py
+++ da_vinci.py
@@ -5,10 +5,12 @@ from voice_cog import VoiceCog
 
 bot = commands.Bot(command_prefix='$')
 
+voice_cog = VoiceCog(bot)
+bot.add_cog(voice_cog)
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
-    bot.add_cog(VoiceCog(bot))
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -18,7 +20,7 @@ async def on_voice_state_update(member, before, after):
     if after.channel.id == 807845575730659348:
         print(f"{member.name} joined the bathroom")
         vc = await after.channel.connect()
-        await voice_client_say("You look cute", vc)
+        voice_cog.say_text("You look cute", vc)
 
 @bot.command()
 async def ping(ctx):
