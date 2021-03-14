22198f28aac2ee32832a16dc330bd6f875a18d13
Dash Brittain <roxanne.brittain@gmail.com>
2021-03-13 16:40:53 -0800

    Fix ID

--- channels.py
+++ channels.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class VoiceChannel(Enum):
     cyberdelia_bathroom = 807845575730659348
-    school_bathroom = 807845575730659348
+    school_bathroom = 813135055496413249
     kates_computer = 807833641564373012
     roof_pool = 807841488729276457
 
